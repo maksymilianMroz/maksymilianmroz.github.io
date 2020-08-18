82b935e486f4d2200785e3b0423b11876eb5fd1f
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-19 01:45:23 +0200

    Updates

---
+++ precache-manifest.1ac8e3e2b5bb09b8c8cde6790a9112f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718be1f9defdb0081c3edebfe19266d7",
+    "url": "/maksymilianmroz.github.io/index.html"
+  },
+  {
+    "revision": "52f2245aa22b39350dd3",
+    "url": "/maksymilianmroz.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "28f51a002a248151acbb",
+    "url": "/maksymilianmroz.github.io/static/js/2.6b160c69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maksymilianmroz.github.io/static/js/2.6b160c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f2245aa22b39350dd3",
+    "url": "/maksymilianmroz.github.io/static/js/main.fb2c855e.chunk.js"
+  },
+  {
+    "revision": "86c9eead19b129267e30",
+    "url": "/maksymilianmroz.github.io/static/js/runtime-main.2f907ece.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/maksymilianmroz.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
