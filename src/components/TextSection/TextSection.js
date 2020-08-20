d60cbc7c3f9edd7dc867854dafb7c9f2f9707aa8
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-21 00:20:30 +0200

    Template code for TextSection component added

---
+++ src/components/TextSection/TextSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './TextSection.module.scss';;
+
+const TextSection = () => {
+    return (
+        <div className={classes.TextSection}>
+            test
+        </div>
+    );
+};
+
+export default TextSection;
