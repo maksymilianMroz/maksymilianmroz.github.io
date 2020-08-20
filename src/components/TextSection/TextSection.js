4c0cb929f44a0fb31e74e9eb57da5d52056faadd
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-21 01:18:49 +0200

    Some styles added

--- src/components/TextSection/TextSection.js
+++ src/components/TextSection/TextSection.js
@@ -4,9 +4,9 @@ import classes from './TextSection.module.scss';;
 const TextSection = () => {
     return (
         <div className={classes.TextSection}>
-            <div className="section">
-                <h1 className="section__header">Lorem Ipsum</h1>
-                <p className="section__content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu orci lorem. Duis varius risus semper massa vestibulum, tincidunt placerat sapien fermentum. Duis nec ultricies eros. In sed quam quis arcu egestas egestas sed eu mi. Suspendisse egestas ullamcorper orci, ac congue felis luctus quis.</p>
+            <div className={classes.section}>
+                <h1 className={classes.section__header}>Lorem Ipsum</h1>
+                <p className={classes.section__content}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu orci lorem. Duis varius risus semper massa vestibulum, tincidunt placerat sapien fermentum. Duis nec ultricies eros. In sed quam quis arcu egestas egestas sed eu mi. Suspendisse egestas ullamcorper orci, ac congue felis luctus quis.</p>
             </div>
         </div>
     );
