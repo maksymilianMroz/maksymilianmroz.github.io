3dca48747366f9466b4aaaeca37690a48310742a
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-21 02:00:13 +0200

    New styles and content for text component

--- src/components/TextSection/TextSection.js
+++ src/components/TextSection/TextSection.js
@@ -6,7 +6,8 @@ const TextSection = () => {
         <div className={classes.TextSection}>
             <div className={classes.section}>
                 <h1 className={classes.section__header}>Lorem Ipsum</h1>
-                <p className={classes.section__content}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu orci lorem. Duis varius risus semper massa vestibulum, tincidunt placerat sapien fermentum. Duis nec ultricies eros. In sed quam quis arcu egestas egestas sed eu mi. Suspendisse egestas ullamcorper orci, ac congue felis luctus quis.</p>
+                <p className={classes.section__content__one}>My name is Max. I love coding.</p>
+                <p className={classes.section__content__two}>I live in Warsaw. Every day I try to learn as much as possible to be better frontend developer. Find out more about me! 🔥</p> 
             </div>
         </div>
     );
