78add7b726ae7c76964617e792ae0ad330248707
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-21 02:12:48 +0200

    Version 1.0 of text component done

--- src/components/TextSection/TextSection.js
+++ src/components/TextSection/TextSection.js
@@ -5,7 +5,7 @@ const TextSection = () => {
     return (
         <div className={classes.TextSection}>
             <div className={classes.section}>
-                <h1 className={classes.section__header}>Lorem Ipsum</h1>
+                <h1 className={classes.section__header}>HEY <br/> THERE.</h1>
                 <p className={classes.section__content__one}>My name is Max. I love coding.</p>
                 <p className={classes.section__content__two}>I live in Warsaw. Every day I try to learn as much as possible to be better frontend developer. Find out more about me! 🔥</p> 
             </div>
