f59eea6cbbcbbad525109f0cc72ac2821343e01e
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-23 15:51:56 +0200

    Template code in PortfolioItem

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './Portfolio.module.scss';
+import PortfolioItem from './PortfolioItem/PortfolioItem';
 
 const Portfolio = () => {
     return (
         <div className={classes.Portfolio}>
             <div className={classes.section}>
                 <h4 className={classes.portfolio__header}>My portfolio!</h4>
+                <PortfolioItem />
             </div>
         </div>
     );
---
+++ src/components/Portfolio/PortfolioItem/PortfolioItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './PortfolioItem.module.scss';
+
+const PortfolioItem = () => {
+    return (
+        <div className={classes.PortfolioItem}>
+            test
+        </div>
+    )
+};
+
+export default PortfolioItem;
