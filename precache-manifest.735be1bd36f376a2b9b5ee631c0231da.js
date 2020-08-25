a141c15a95b9205e13bbc95f137c636e4bc62c97
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-26 01:58:11 +0200

    Updates

---
+++ precache-manifest.735be1bd36f376a2b9b5ee631c0231da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efcc45ee25e34b0321461938d42b571",
+    "url": "/maksymilianmroz.github.io/index.html"
+  },
+  {
+    "revision": "29266f2dc20e6d1e9d45",
+    "url": "/maksymilianmroz.github.io/static/css/main.768f50ae.chunk.css"
+  },
+  {
+    "revision": "610418933ee108bf5ff9",
+    "url": "/maksymilianmroz.github.io/static/js/2.cc0d45e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maksymilianmroz.github.io/static/js/2.cc0d45e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29266f2dc20e6d1e9d45",
+    "url": "/maksymilianmroz.github.io/static/js/main.86362809.chunk.js"
+  },
+  {
+    "revision": "86c9eead19b129267e30",
+    "url": "/maksymilianmroz.github.io/static/js/runtime-main.2f907ece.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/maksymilianmroz.github.io/precache-manifest.340b9353c2e60c3ad22f153b4c0f8083.js"
+  "/maksymilianmroz.github.io/precache-manifest.735be1bd36f376a2b9b5ee631c0231da.js"
 );
 
 self.addEventListener('message', (event) => {
