099e9868e9fe3bb812b99cea67eb3f2e9b10779a
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-25 18:18:53 +0200

    Props in first section upgraded

--- src/components/TextSection/TextSection.js
+++ src/components/TextSection/TextSection.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import classes from './TextSection.module.scss';
 
-const TextSection = () => {
+const TextSection = (props) => {
     return (
         <div className={classes.TextSection}>
             <div className={classes.section}>
-                <h1 className={classes.section__header}>HEY <br/> THERE.</h1>
-                <p className={classes.section__content__one}>My name is Max. I love coding.</p>
-                <p className={classes.section__content__two}>I live in Warsaw. Every day I try to learn as much as possible to be better frontend developer. Find out more about me! 🔥</p> 
+                <h1 className={classes.section__header}>{props.title}</h1>
+                <p className={classes.section__content__one}>{props.firstLine} <span>{props.firstLineDecor}</span></p>
+    <p className={classes.section__content__two}>{props.par} <a href='#'>{props.button}</a> </p>
             </div>
         </div>
     );
--- src/containers/App.js
+++ src/containers/App.js
@@ -6,19 +6,26 @@ import Portfolio from '../components/Portfolio/Portfolio';
 
 class App extends Component {
   state = {
+    title: 'andrzej',
     firstSection: {
       title: 'Hey there.',
       firstLine: 'My name is Max.',
       firstLineDecor: 'I love coding.',
       par: 'I live in Warsaw. Every day I try to learn as much as possible to be better frontend developer.',
-      button: 'Find out more about me!'
+      button: 'Find out more about me! 🔥'
     }
   }
 
   render() {
     return (
       <div className="App">
-        <TextSection />
+        <TextSection 
+        title={this.state.firstSection.title}
+        firstLine={this.state.firstSection.firstLine}
+        firstLineDecor={this.state.firstSection.firstLineDecor}
+        par={this.state.firstSection.par}
+        button={this.state.firstSection.button}
+        />
         <Portfolio />
         <TextSection />
       </div>
