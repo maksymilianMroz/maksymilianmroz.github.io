1acb922b3b27b3d9f891bdd2d4f3d4b5a10e7384
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-25 16:30:03 +0200

    Change App to class component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 
 import TextSection from '../components/TextSection/TextSection';
 import Portfolio from '../components/Portfolio/Portfolio';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <TextSection />
@@ -12,6 +13,7 @@ function App() {
       <TextSection />
     </div>
   );
+}
 }
 
 export default App;
