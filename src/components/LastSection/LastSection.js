e3629d1ef9c40cf1d8a6a6fe18fc2706b9adae0a
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-26 01:53:15 +0200

    Last section fullfiled with data from state

--- src/components/LastSection/LastSection.js
+++ src/components/LastSection/LastSection.js
@@ -4,8 +4,10 @@ import classes from './LastSection.module.scss';
 const LastSection = (props) => {
     return (
         <div className={classes.LastSection}>
-            <div className={classes.section}>
-                <h2 className={classes.header}>{props.title}</h2>
+            <div className={classes.contactContainer}>
+                <h2 className={classes.contactContainer__header}>{props.title}</h2>
+                <p className={classes.contactContainer__info}>{props.info}</p>
+                <p className={classes.contactContainer__shortSendText}>{props.shortSendText} <a href="#" className={classes.contactContainer__mailAdress}>{props.mailAdress}</a></p>
             </div>
         </div>
     );
