862cf21b0a8728fae1c91a458adb687616929f25
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-25 19:42:21 +0200

    Text section style upgrade

--- src/components/TextSection/TextSection.js
+++ src/components/TextSection/TextSection.js
@@ -6,8 +6,8 @@ const TextSection = (props) => {
         <div className={classes.TextSection}>
             <div className={classes.section}>
                 <h1 className={classes.section__header}>{props.title}</h1>
-                <p className={classes.section__content__one}>{props.firstLine} <span>{props.firstLineDecor}</span></p>
-    <p className={classes.section__content__two}>{props.par} <a href='#'>{props.button}</a> </p>
+                <p className={classes.section__content__one}>{props.firstLine} <span className={classes.section__content__one_color}>{props.firstLineDecor}</span></p>
+                <p className={classes.section__content__two}>{props.par} <a href='#'>{props.button}</a> </p>
             </div>
         </div>
     );
