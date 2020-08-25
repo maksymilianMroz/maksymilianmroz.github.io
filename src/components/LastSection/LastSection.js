7cc8dc1a17a9b8feb37a7fac7f6f75ce4202e5b0
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-26 00:24:02 +0200

    Some styles added to last section

--- src/components/LastSection/LastSection.js
+++ src/components/LastSection/LastSection.js
@@ -4,7 +4,9 @@ import classes from './LastSection.module.scss';
 const LastSection = () => {
     return (
         <div className={classes.LastSection}>
-            test
+            <div className={classes.section}>
+                test
+            </div>
         </div>
     );
 };
