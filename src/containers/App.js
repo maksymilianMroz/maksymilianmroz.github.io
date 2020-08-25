1d6491ac4cfd524d9a68af59ec77b1fa0213471a
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-25 17:42:04 +0200

    State for first section update

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,7 +5,15 @@ import TextSection from '../components/TextSection/TextSection';
 import Portfolio from '../components/Portfolio/Portfolio';
 
 class App extends Component {
-
+  state = {
+    firstSection: {
+      title: 'Hey there.',
+      firstLine: 'My name is Max.',
+      firstLineDecor: 'I love coding.',
+      par: 'I live in Warsaw. Every day I try to learn as much as possible to be better frontend developer.',
+      button: 'Find out more about me!'
+    }
+  }
 
   render() {
     return (
