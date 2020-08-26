230be184c9e87f70da6f00b6a771babfc30d1404
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-26 17:11:36 +0200

    First section styles done.

--- src/components/TextSection/TextSection.js
+++ src/components/TextSection/TextSection.js
@@ -7,7 +7,7 @@ const TextSection = (props) => {
             <div className={classes.section}>
                 <h1 className={classes.section__header}>{props.title}</h1>
                 <p className={classes.section__content__one}>{props.firstLine} <span className={classes.section__content__one_color}> {props.firstLineDecor}</span></p>
-                <p className={classes.section__content__two}>{props.par} <a href='#'>{props.button}</a> </p>
+                <p className={classes.section__content__two}>{props.par} <a href='#' className={classes.section__content__two_link}>{props.button}</a> </p>
             </div>
         </div>
     );
