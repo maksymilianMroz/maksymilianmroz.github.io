94f12abcde3f9c43f9ffb50b6b440b9cd7f3865a
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2020-08-26 14:42:16 +0200

    Animation on the bordered text in first section done

--- src/components/TextSection/TextSection.js
+++ src/components/TextSection/TextSection.js
@@ -6,7 +6,7 @@ const TextSection = (props) => {
         <div className={classes.TextSection}>
             <div className={classes.section}>
                 <h1 className={classes.section__header}>{props.title}</h1>
-                <p className={classes.section__content__one}>{props.firstLine} <span className={classes.section__content__one_color}>{props.firstLineDecor}</span></p>
+                <p className={classes.section__content__one}>{props.firstLine} <span className={classes.section__content__one_color}> {props.firstLineDecor}</span></p>
                 <p className={classes.section__content__two}>{props.par} <a href='#'>{props.button}</a> </p>
             </div>
         </div>
